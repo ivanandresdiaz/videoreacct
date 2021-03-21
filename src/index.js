7c1ece7a15cc1112e04de7d00a7431401d20544c
Andres Diaz <ivanandresdilox@gmail.com>
2021-03-21 16:08:05 -0500

    entorno de trabajo listo

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/styles/App.scss';
+import HelloWorld from './component/HelloWorld';
+const App= document.getElementById('app');
+ReactDOM.render(<HelloWorld/>,App);
